7fc21b0d60d79178cd186e784f72fc3c789ecd58
Andrey Tkachuk <chkaayendutrk@gmail.com>
2023-10-02 20:19:09 +0300

    1.0.1 * implemented caching of fetched repos * implemented writing the report to md file * adjusted README.md

--- README.md
+++ README.md
@@ -1,21 +1,95 @@
 # gitlab-global-search
 
- node.js script which provides a possibility to search over all GitLab repositories files for some search term  
+ node.js script which provides a possibility to search over all GitLab repositories files for some 
+ search term. Based on usage of native GitLab API.
 
 ## Used dependencies
 + requests 
 + fs 
 
 ## Setting up the project
-```
+```shell
 npm i
 ```
 
 ## Running the script 
+
+```shell
+node gitlab-search.js --search=<search-term> --repo=https://test.gitlab.com --access-token=<acc-token> --raw --cache-repos
 ```
-node gitlab-search.js --search=<search-term> --repo=https://test.gitlab.com --access-token=<acc-token>
+
+where
++ `search` — search term;
++ `repo` — GitLab URL;
++ `access-token` — Access token for your GitLab account;
++ `raw` — (Optional) Determines the way of writing the report data: in raw `.txt` file or in `.md` file.
++ `cache-repos` — (Optional) If enabled, stores fetched repositories in a separate file in `output/repos.json`.
+
+To avoid inputting the data about the repo and access token, those data can be stored as environment 
+variables under the `REPO` and `ACCESS_TOKEN` keys. </br>
+
+So, basically for IntelliJ configuration can look as follows.
+![img.png](static/intellij-config-example.png)
+
+### Result of script 
+
+After starting the script you will be able to see the console output of running the program: 
+```shell
+
+   ____ _ _   _       _         ____                      _     
+  / ___(_) |_| | __ _| |__     / ___|  ___  __ _ _ __ ___| |__  
+ | |  _| | __| |/ _` | '_ \    \___ \ / _ \/ _` | '__/ __| '_ \ 
+ | |_| | | |_| | (_| | |_) |    ___) |  __/ (_| | | | (__| | | |
+  \____|_|\__|_|\__,_|_.__/    |____/ \___|\__,_|_|  \___|_| |_|
+                                                                           v 1.0.1                                     
+
+============== START PROJECTS FETCHING ==============
+Caching enabled. Read 28 projects from cache file
+========= START SEARCHING OVER THE PROJECTS =========
+Done. 2 matching projects have been written to result.txt file
+
 ```
-Where
-+ --search — search term;
-+ --repo — GitLab URL;
-+ --access-token — Access token for your GitLab account 
+
+### Output data
+
+After running the script you will be able to check the report in the way that you've selected using `--raw` 
+parameter. Both `result.md` and `result.txt` files will be available in `output` directory. <br/>
+
+#### Case: search term _dependency_
+
+Example of `result.txt` file (when `raw` parameter was provided):
+
+```shell
+PROJECT URL: https://test.gitlab.com/java-microservices-test
+- branch: develop
+  path: src/main/java/com/example/Main.java
+  line: 31
+- branch: develop
+  path: src/main/java/com/example/Main.java
+  line: 34
+```
+Example of `result.md` file:
+
+![img.png](static/report-md-example.png)
+
+## Caching
+As  [mentioned before](#Running-the-script) you can store fetched GitLab repositories in a cache 
+file to avoid repeating fetching info of repos before each search. After you provide `--cache-repos` 
+parameter during the first time script will work as usual. When you will try to run it once more, you 
+will notice in console that now it uses cache file for getting the info about the repos. This file will 
+be located in the `output` directory under the `repos.json` name. <br/>
+Here goes an example of such file:
+```shell
+[{"id":1,"web_url":"https://test.gitlab.com/java-microservices-test"}]
+```
+
+## Final: Inspiration of the project
+I suppose all of us, Software Engineers, at least once faced the situation when there was a need to search
+over GitLab projects not for some specific repo, but for some code inside all the repos. Interesting fact
+that by default GitLab provides only the possibility to search over only one specific project and searching 
+over all project requires involving of SysAdmins DevOps and other magic guys to configure GitLab server with 
+such features as Elasticsearch and Advanced Search while the feature seems like from the category 
+of out-of-the-box ones. Even ancient SVN provides such possibility by default, just imagine! 
+So, the idea of that script was to provide as much as easy way to short your time
+while searching for some code snippets over your huge GitLab with a plenty of Repositories. Project is open
+for any ideas and adjustments, so feel free to create some issues, MRs and so on. Good Luck and enjoy!
--- src/gitlab-search.js
+++ src/gitlab-search.js
@@ -1,11 +1,22 @@
 const request = require('request');
 const fs = require('fs');
 
+console.log(`
+   ____ _ _   _       _         ____                      _     
+  / ___(_) |_| | __ _| |__     / ___|  ___  __ _ _ __ ___| |__  
+ | |  _| | __| |/ _\` | '_ \\    \\___ \\ / _ \\/ _\` | '__/ __| '_ \\ 
+ | |_| | | |_| | (_| | |_) |    ___) |  __/ (_| | | | (__| | | |
+  \\____|_|\\__|_|\\__,_|_.__/    |____/ \\___|\\__,_|_|  \\___|_| |_|
+                                                                           v 1.0.1                                     
+`);
+
 const args = process.argv.slice(2);
 
 let baseUrl = process.env['REPO'];
 let accessToken = process.env['ACCESS_TOKEN'];
 let searchStr;
+let isRawReport = false;
+let cacheRepos = false;
 
 args.forEach(arg => {
     if (arg.startsWith('--access-token')) {
@@ -20,42 +31,66 @@ args.forEach(arg => {
         searchStr = arg.split('=')[1];
     }
 
+    if (arg.startsWith('--raw')) {
+        isRawReport = true;
+    }
+
+    if (arg.startsWith('--cache-repos')) {
+        cacheRepos = true;
+    }
 });
 
+const OUTPUT_DIR = './output';
+const OUTPUT_FILE = `${OUTPUT_DIR}/${isRawReport && './result.txt' || './result.md'}`;
+const CACHE_REPOS_FILE = `${OUTPUT_DIR}/repos.json`;
+fs.existsSync(OUTPUT_FILE) && fs.unlinkSync(OUTPUT_FILE);
+
 if (!searchStr) {
-    throw new Error('No search string passed!');
+    console.error('[ERROR] No search string passed! Please provide it via --search parameter');
+    process.exit(1);
 }
 
 if (!baseUrl) {
-    throw new Error('No GitLab url passed!');
+    console.error('[ERROR] No GitLab url passed! Please provide it via --repo parameter');
+    process.exit(1);
 }
 
 if (!accessToken) {
-    throw new Error('No access token for GitLab passed!');
+    console.error('[ERROR] No access token for GitLab passed! Please provide it via --access-token parameter');
+    process.exit(1);
 }
 
-const getProjectsUrl = (pageNum) => `${baseUrl}/api/v4/projects?access_token=${accessToken}&per_page=500&page=${pageNum}`;
+const getProjectsUrl = (pageNum) => `${baseUrl}/api/v4/projects?access_token=${accessToken}&per_page=100&page=${pageNum}`;
 const getSearchInProjectUrl = (projectId) => `${baseUrl}/api/v4/projects/${projectId}/search?access_token=${accessToken}&scope=blobs&search=${searchStr}`;
 
 let projects = [];
 
 const getProjects = (pageNum, callback) => {
-    request.get(
-        getProjectsUrl(pageNum),
-        (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                const newProjects = JSON.parse(body);
+    if (cacheRepos && fs.existsSync(CACHE_REPOS_FILE)) {
+        projects = JSON.parse(fs.readFileSync(CACHE_REPOS_FILE));
+        console.log(`Caching enabled. Read ${projects.length} projects from cache file`);
+        callback.call();
+    } else {
+        request.get(
+            getProjectsUrl(pageNum),
+            (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const newProjects = JSON.parse(body);
 
-                if (newProjects.length > 0) {
-                    projects = [...projects, ...newProjects];
-                    getProjects(++pageNum, callback);
-                } else {
-                    console.log(`Done. Found ${projects.length} projects on ${baseUrl}`);
-                    callback.call();
+                    if (newProjects.length > 0) {
+                        projects = [...projects, ...newProjects];
+                        getProjects(++pageNum, callback);
+                    } else {
+                        console.log(`Done. Found ${projects.length} projects on ${baseUrl}`);
+                        cacheRepos && fs.writeFileSync(CACHE_REPOS_FILE, JSON.stringify(
+                            projects.map(({id, web_url}) => ({id, web_url}))
+                        ));
+                        callback.call();
+                    }
                 }
             }
-        }
-    );
+        );
+    }
 }
 
 const searchOverProject = () => {
@@ -73,15 +108,25 @@ const searchOverProject = () => {
                     if (searchOccurences.length > 0) {
                         matchedProjects++;
                         let repoData = `PROJECT URL: ${project['web_url']}`;
-                        searchOccurences.forEach(({ ref, path, startline }) => {
-                            repoData += `\n- branch: ${ref}`;
-                            repoData += `\n  path: ${path}`;
-                            repoData += `\n  line: ${startline}`;
+                        searchOccurences.forEach(({ref, path, startline, data}) => {
+                            if (isRawReport) {
+                                repoData += `\n- branch: ${ref}`;
+                                repoData += `\n  path: ${path}`;
+                                repoData += `\n  line: ${startline}`;
+                            } else {
+                                repoData += `\n- branch: \`${ref}\` <br/>`;
+                                repoData += `\n  path: \`${path}\` <br/>`;
+                                repoData += `\n  line: \`${startline}\` <br/>`;
+                                repoData +=
+                                    '\n  occurrence:' +
+                                    '\n```' + data + '```'
+                                ;
+                            }
                         });
                         repoData += '\n\n';
                         // for debug
-                        // console.log(repoData);
-                        fs.appendFileSync('./result.txt', repoData);
+                        // console.debug(repoData);
+                        fs.appendFileSync(OUTPUT_FILE, repoData);
                     }
                 }
                 if (checkedProjectsAmt === projects.length) {
